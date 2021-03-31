78efd7286ff39724cd9bfd0fde2aee0437df6304
ZYR <75359804+ZaidRather@users.noreply.github.com>
2021-03-31 20:30:40 +0530

    assQ36

---
+++ WriteCSV36.c
@@ -0,0 +1,34 @@
+//ZaidRather
+//Program to write .csv files.
+#include <stdio.h> //Including standard input output header files.
+#include <stdlib.h>//Including standard library header files.
+#include <string.h>//Including string header files. 
+int main() 
+{ 
+    FILE* fp = fopen("C:\\Users\\LENOVO\\Desktop\\cprograms\\YogitaTufchi_118_C_Program_Repository\\write.CSV", "a+"); //File path.
+
+    char name[50]; 
+    int rollno, marks; 
+
+    if (!fp) { 
+        // Error in file opening 
+        printf("Can't open file\n"); 
+        return 0; 
+    }
+    // Asking user input for the new record to be added.   
+    printf("\nEnter Student Name\n"); 
+    scanf("%s", &name); 
+    printf("\nEnter Roll Number\n"); 
+    scanf("%d", &rollno); 
+    printf("\nEnter Attendance\n"); 
+    scanf("%d", &marks); 
+
+    // Saving data in file 
+    fprintf(fp, "%s, %d, %d\n", name, 
+            rollno, marks); 
+
+    printf("\nNew student deatils added."); 
+
+    fclose(fp); 
+    return 0; 
+}
