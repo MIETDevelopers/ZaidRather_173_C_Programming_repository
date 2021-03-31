f5a2ab432e9c9ed0d57cf32d4654e906bcbd2544
ZYR <75359804+ZaidRather@users.noreply.github.com>
2021-03-31 20:56:54 +0530

    assQ4

---
+++ 3_Hello.c
@@ -0,0 +1,9 @@
+//Name: ZaidRather
+//Program:Hello World Program
+
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
+}
